1be9be0b0f1ca7d92c82939ab704c9c85c15920c
dgekaa <dgeka_vip@mail.ru>
2020-02-28 11:48:52 +0300

    Updates

---
+++ precache-manifest.818aca5491cc85b8bd92362c4fa8490f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d5decc8a4462beab766b980063a698",
+    "url": "/party_live/index.html"
+  },
+  {
+    "revision": "cfaa6f5843cf9441c1cd",
+    "url": "/party_live/static/css/main.73ef7577.chunk.css"
+  },
+  {
+    "revision": "5cf9191c119303b9f4c9",
+    "url": "/party_live/static/js/2.8b3bda53.chunk.js"
+  },
+  {
+    "revision": "fc6b05d7581fe925877ed2c20af4acda",
+    "url": "/party_live/static/js/2.8b3bda53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfaa6f5843cf9441c1cd",
+    "url": "/party_live/static/js/main.45519483.chunk.js"
+  },
+  {
+    "revision": "11c726b9ce0734d9eafe",
+    "url": "/party_live/static/js/runtime-main.558fe980.js"
+  }
+]);
