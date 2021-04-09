cb53c3c917b410aa0301e9ec33af36987f9591b7
dgekaa <dgeka_vip@mail.ru>
2021-04-09 17:44:15 +0300

    fix

---
+++ build/precache-manifest.0f7cefcfcf6ec9c11adc84441b357329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7963b164c0449c54567ac9cfc0d94035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df0b6d069b720a46223",
+    "url": "/static/css/2.f669bca1.chunk.css"
+  },
+  {
+    "revision": "8cd0e5280d1d97ee5cf1",
+    "url": "/static/css/main.c1774680.chunk.css"
+  },
+  {
+    "revision": "9df0b6d069b720a46223",
+    "url": "/static/js/2.a703c727.chunk.js"
+  },
+  {
+    "revision": "17618a5b134baeb115e44eca7a099e18",
+    "url": "/static/js/2.a703c727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd0e5280d1d97ee5cf1",
+    "url": "/static/js/main.fc9de006.chunk.js"
+  },
+  {
+    "revision": "61502e615d43391eab49",
+    "url": "/static/js/runtime-main.a6c80ad4.js"
+  }
+]);
